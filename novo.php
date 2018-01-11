1fbbf1b9a61874cd7edd219c4a146f6705b1f860
dan-fortunato <danielfortunato_az@yahoo.com.br>
2018-01-10 23:25:26 -0200

    formulario simples em HTML

---
+++ novo.php
@@ -0,0 +1,13 @@
+<head>
+<title>Calculadora PHP</title>
+</head>
+<body>
+<form action="calculadora.php" method="post">
+	Digite o primeiro Número<input name="primeiro" type="text"><br>
+	Digite o segundo Número<input name="segundo" type="text"><br>
+	<input name="operacao" type="submit" value="+">	
+	<input name="operacao" type="submit" value="-">	
+	<input name="operacao" type="submit" value="*">	
+	<input name="operacao" type="submit" value="/">	
+</body>
+</form>
